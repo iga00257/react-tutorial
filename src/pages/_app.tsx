98578ac3d3393bed8e768e01827c9b411825f153
劉穎多 <liuyingduo@Mac-mini.local>
2024-03-03 15:06:20 +0800

    feat: update app.tsx & index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+
+// Styles
+import '@/styles/main.scss'
+import '@/styles/Icon/index.scss'
+import '@/styles/motion/index.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

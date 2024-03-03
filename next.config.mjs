bf86b5051890d6ae14c0bf3866064221bc3cea96
劉穎多 <liuyingduo@Mac-mini.local>
2024-03-03 12:00:00 +0800

    feat: add project basic setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    additionalData: '@import "./src/styles/_variables.scss";@import "./src/styles/mixins/mixins.scss";',
+  },
 };
 
 export default nextConfig;
